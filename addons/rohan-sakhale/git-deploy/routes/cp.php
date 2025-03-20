2b75a37047817ee088efcd6e727dad75a203598c
Rohan Sakhale <rohansakhale@gmail.com>
2025-03-20 13:47:56 +0530

    Remove addon

--- addons/rohan-sakhale/git-deploy/routes/cp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use RohanSakhale\GitDeploy\Http\Controllers\DeployController;
-
-Route::middleware('statamic.cp.auth')->group(function () {
-    Route::get('/git-deploy', [DeployController::class, 'index'])->name('statamic.cp.git-deploy.index');
-    Route::post('/git-deploy/push', [DeployController::class, 'commitAndPush'])->name('statamic.cp.git-deploy.push');
-});
